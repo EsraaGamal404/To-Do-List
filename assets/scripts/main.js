e4150aef6e14577e6c21b95a77f7b922e61fd17d
esraa gamal <eg6237561@gmail.com>
2025-01-13 22:17:08 +0200

    Start: Project and create Folder Structure and Add: Assets

---
+++ assets/scripts/main.js
@@ -0,0 +1,31 @@
+
+  
+let taskList = "";
+if (localStorage.getItem("tasks") !== null) {
+  taskList = localStorage.getItem("tasks");
+}
+function addTask() {
+  let taskInput = document.getElementById("task-input");
+  let taskStatus = "In progress";
+  taskList += `
+    <li>${taskInput.value}<br>
+    ${new Date().toLocaleString()}| ${taskStatus}</li>
+  `;
+  localStorage.setItem("tasks", taskList);
+  taskInput.value = "";
+  showTask();
+}
+function showTask() {
+  document.getElementById("task-list").innerHTML = localStorage.getItem("tasks");
+}
+showTask();
+document.getElementById("add-task-btn").onclick = () => addTask();
+
+
+document.getElementById("switch").onclick = () => {
+  if (document.body.classList.contains("dark-theme")) {
+    document.body.classList.remove("dark-theme");
+  } else {
+    document.body.classList.add("dark-theme");
+  }
+};
