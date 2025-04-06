d55f2a48e9c31dd6c7a21a133eb0bf55b9511425
Esraa gamal <eg6237561@gmail.com>
2025-04-06 02:21:12 +0200

    Added compiled Js file

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,31 +1,24 @@
-
-  
-let taskList = "";
-if (localStorage.getItem("tasks") !== null) {
-  taskList = localStorage.getItem("tasks");
-}
 function addTask() {
   let taskInput = document.getElementById("task-input");
   let taskStatus = "In progress";
-  taskList += `
-    <li>${taskInput.value}<br>
-    ${new Date().toLocaleString()}| ${taskStatus}</li>
+
+  let newTask = document.createElement("li");
+  newTask.innerHTML = `
+    ${taskInput.value}<br>
+    ${new Date().toLocaleString()} | ${taskStatus}
   `;
-  localStorage.setItem("tasks", taskList);
+  document.getElementById("task-list").appendChild(newTask);
+
+  let savedTasks = localStorage.getItem("tasks") || "";
+  savedTasks += newTask.outerHTML;
+  localStorage.setItem("tasks", savedTasks);
+
   taskInput.value = "";
-  showTask();
 }
-function showTask() {
-  document.getElementById("task-list").innerHTML = localStorage.getItem("tasks");
-}
-showTask();
+
 document.getElementById("add-task-btn").onclick = () => addTask();
 
-
 document.getElementById("switch").onclick = () => {
-  if (document.body.classList.contains("dark-theme")) {
-    document.body.classList.remove("dark-theme");
-  } else {
-    document.body.classList.add("dark-theme");
-  }
+  document.body.classList.toggle("dark-theme");
 };
+
---
+++ assets/scripts/typescript/main.js
@@ -0,0 +1,19 @@
+"use strict";
+// function addTask() {
+//   let taskInput = document.getElementById("task-input");
+//   let taskStatus = "In progress";
+//   let newTask = document.createElement("li");
+//   newTask.innerHTML = `
+//     ${taskInput.value}<br>
+//     ${new Date().toLocaleString()} | ${taskStatus}
+//   `;
+//   document.getElementById("task-list").appendChild(newTask);
+//   let savedTasks = localStorage.getItem("tasks") || "";
+//   savedTasks += newTask.outerHTML;
+//   localStorage.setItem("tasks", savedTasks);
+//   taskInput.value = "";
+// }
+// document.getElementById("add-task-btn").onclick = () => addTask();
+// document.getElementById("switch").onclick = () => {
+//   document.body.classList.toggle("dark-theme");
+// };
